a3dad90ae08495bffa444e015106359f7bd63f05
Tikos1 <1987830777@qq.com>
2023-12-13 17:28:20 +0800

    精准哨兵

--- vue.config.js
+++ vue.config.js
@@ -4,6 +4,19 @@ module.exports = defineConfig({
   // 设置正向代理服务器
   devServer: {
     proxy: 'http://111.229.247.11:3000'
-  }
+  },
+  configureWebpack: {
+    // devtool
+    devtool: "source-map", // Source map generation must be turned on
+    plugins: [
+      sentryWebpackPlugin({
+        org: "Tiko",
+        project: "vue-sentry",
+        // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+        authToken:
+          "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",
+      }),
+    ],
+  },
 })
 //http://111.229.247.11:3000
